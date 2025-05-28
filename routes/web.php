85be44f476a3c5990868e0ce0600b9e6c6ab3fc9
Basanta Chapagain <instructor.basanta@gmail.com>
2025-05-28 11:21:12 +0545

    Auth Tempalte Setup

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,33 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+
+Route::get('/login', function () {
+    return view('backend.user.login');
+});
+
+Route::get('/register', function () {
+    return view('backend.user.register');
+});
+
+Route::get('/forgot-password', function () {
+    return view('backend.user.forgot_password');
+});
+
+Route::get('/backend/dashboard', function () {
+    return view('backend.dashboard.index');
+});
+
+Route::get('/backend/dashboard', function () {
+    return view('backend.dashboard.index');
+});
+
+Route::get('/backend/category/create', function () {
+    return view('backend.category.create');
+});
+
+Route::get('/backend/category', function () {
+    return view('backend.category.index');
+});
